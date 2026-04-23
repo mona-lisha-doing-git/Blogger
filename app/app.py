d273b47e951b90e04f9bfe3fed40831ec480a441
Amazon-Clone <monalisha.k559@gmail.com>
2026-04-24 01:57:02 +0530

    Uploading and Retrieving

--- app/app.py
+++ app/app.py
@@ -1,49 +1,52 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import create_db_and_tables, Post, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
-text_posts = {
-    1: {"title": "Getting Started with FastAPI", "content": "FastAPI is a modern web framework for building APIs with Python."},
-    2: {"title": "Understanding REST APIs", "content": "REST APIs use HTTP methods like GET, POST, PUT, DELETE."},
-    3: {"title": "Python Tips", "content": "Use list comprehensions and generators for efficient code."},
-    4: {"title": "Async vs Sync", "content": "Async programming helps handle multiple requests efficiently."},
-    5: {"title": "Databases with SQLAlchemy", "content": "SQLAlchemy is a powerful ORM for Python."},
-    6: {"title": "JWT Authentication", "content": "JWT is used for secure authentication between client and server."},
-    7: {"title": "Error Handling in APIs", "content": "Always return proper HTTP status codes and messages."},
-    8: {"title": "Project Ideas", "content": "Build a password manager or blog API using FastAPI."},
-    9: {"title": "Deployment Guide", "content": "You can deploy FastAPI apps using Docker, Render, or GCP."},
-    10: {"title": "Debugging Tips", "content": "Use logs and print statements to debug your application."}
-}
+@app.post('/upload')
+async def upload_file(
+        file: UploadFile = File(...),
+        caption: str = Form(""),
+        session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption=caption,
+        url = "dummy url",
+        file_type = "photo",
+        file_name = "dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
 
-@app.get("/posts")
-def get_all_post(limit: int = None):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
+@app.get('/feed')
+async def get_feed(
+        session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
 
-@app.get("/posts/{id}")
-def get_post(id: int):
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return text_posts.get(id)
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
 
-@app.post("/posts")
-def create_post(post: PostCreate) -> PostResponse:
-    new_post = {"title":post.title, "content":post.content}
-    text_posts[max(text_posts.keys()) + 1] = new_post
-    return new_post
-
-@app.delete("/posts/{id}")
-def delete_post(id: int):
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post not found!!!!")
-    del_post = text_posts[id]
-    del text_posts[id]
-    return del_post
+    return {"posts": posts_data}
--- app/db.py
+++ app/db.py
@@ -7,9 +7,12 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 
-DATABASE_URL = "sqlite_aiosqlite:///./test.db"
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-class Post(DeclarativeBase): # so that it knows that are making a data model
+class Base(DeclarativeBase):
+    pass
+
+class Post(Base): # so that it knows that are making a data model
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -17,15 +20,15 @@ class Post(DeclarativeBase): # so that it knows that are making a data model
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
-    created_at = Column(String, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
-        yield session 
+        yield session
